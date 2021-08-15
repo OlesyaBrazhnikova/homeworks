04b25931eeb520391bae1f37e6209e74dd446b6d
Olesya <olesyabrazhnikova@ukr.net>
2021-08-15 23:38:21 +0300

    code formatting

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**Task 1**/
 function counter (num) {
-	let result = num;
-	counter = function() {
-		let arg = arguments[0] ? arguments[0] : 0;
-		return result = result + arg;
-	};
-	return counter();
+    let result = num;
+    counter = function() {
+        let arg = arguments[0] ? arguments[0] : 0;
+        return result = result + arg;
+    };
+    return counter();
 }
 console.log(counter(3));
 console.log(counter(5));
@@ -14,16 +14,16 @@ console.log(counter(228));
 
 /**Task 2**/
 function getUpdateArr(){
-	let result = [];
-	getUpdateArr = function(value) {
-		if(value === null || value === undefined) {
-			result = [];
-		} else {
-			result.push(value);
-		}
-		return result;
-	}
-	return getUpdateArr();
+    let result = [];
+    getUpdateArr = function(value) {
+        if(value === null || value === undefined) {
+            result = [];
+        } else {
+            result.push(value);
+        }
+        return result;
+    }
+    return getUpdateArr();
 }
 console.log(getUpdateArr(3));
 console.log(getUpdateArr(5));
@@ -35,16 +35,16 @@ console.log(getUpdateArr(4));
 /**Task 3**/
 function getTime() {
 let endTime,
-	startTime = 0;
-	getTime = function() {
-		if (startTime === 0) {
-			startTime = new Date().getTime();
-			return 'Enabled';
-		} else {
-			endTime = Math.round((new Date().getTime() - startTime)/ 1000);
-			startTime = new Date().getTime();
-			return endTime;
-		}
-	}
+    startTime = 0;
+    getTime = function() {
+        if (startTime === 0) {
+            startTime = new Date().getTime();
+            return 'Enabled';
+        } else {
+            endTime = Math.round((new Date().getTime() - startTime)/ 1000);
+            startTime = new Date().getTime();
+            return endTime;
+        }
+    }
 }
 getTime();
